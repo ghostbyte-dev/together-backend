3c084e71a807887db52389fb10f7e8ebe4122a60
Hiebeler <emanuel.hiebeler@gmail.com>
2022-11-11 16:30:32 +0100

    added test route

--- src/app.js
+++ src/app.js
@@ -21,6 +21,11 @@ app.use('/community', communityRoute)
 const taskRoute = require('./routes/taskRoute')
 app.use('/task', taskRoute)
 
+app.get('test', (req, res) => {
+  console.log('test')
+  res.send('test')
+})
+
 app.listen(3000, () => {
   console.log('App listening on Port 300')
 })
