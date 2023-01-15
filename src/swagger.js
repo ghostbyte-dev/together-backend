a2257f55c7de9e90482ae58fd09492960c49d1f0
Hiebeler <emanuel.hiebeler@gmail.com>
2023-01-15 15:04:22 +0100

    added swagger host from env file

--- src/swagger.js
+++ src/swagger.js
@@ -1,11 +1,12 @@
 const swaggerAutogen = require('swagger-autogen')()
+require('dotenv').config()
 
 const doc = {
   info: {
     title: 'Kommiuniti API',
     description: 'API for Kommiuniti App'
   },
-  host: 'localhost:3000',
+  host: process.env.HOST,
   schemes: ['http'],
   consumes: ['application/json'],
   produces: ['application/json'],
