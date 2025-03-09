a84b1d1a4793a4bf67506f38362514c64eb27b14
daniebeler <hiebeler.daniel@gmail.com>
2025-03-09 23:04:23 +0100

    fix bcrypt

--- src/routes/registrationRoute.js
+++ src/routes/registrationRoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const helper = require('../helper')
 const auth = require('../middleware/userAuth')
 const { PrismaClient } = require('@prisma/client')
