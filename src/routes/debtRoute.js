cd92a0b2f4c3d1efd1e463df3a5f114a84b4a016
Hiebeler <emanuel.hiebeler@gmail.com>
2025-07-22 22:47:50 +0200

    bug fix: get only debts from own community in /balance

--- src/routes/debtRoute.js
+++ src/routes/debtRoute.js
@@ -80,7 +80,8 @@ router.get('/balance', auth, async (req, res) => {
   const moneyToPay = await prisma.debt.groupBy({
     by: ['fk_user_creditor_id'],
     where: {
-      fk_user_debitor_id: req.user.id
+      fk_user_debitor_id: req.user.id,
+      fk_community_id: req.user.communityId
     },
     _sum: {
       amount: true
