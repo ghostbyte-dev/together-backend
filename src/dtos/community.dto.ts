fd669a1c0e28c418b64a624eb71fdd09c63ba8fa
Hiebeler <emanuel.hiebeler@gmail.com>
2025-07-24 17:45:19 +0200

    add code to community dto

--- src/dtos/community.dto.ts
+++ src/dtos/community.dto.ts
@@ -4,10 +4,12 @@ export class CommunityDto {
   id: number;
   name: string;
   admin: UserDto | undefined;
+  code: number;
   // biome-ignore lint/suspicious/noExplicitAny: could be diffrent user types from prisma
   constructor(community: any) {
     this.id = community.id;
     this.name = community.name;
+    this.code = community.code;
     this.admin = community.admin ? new UserDto(community.admin) : undefined;
   }
 }
