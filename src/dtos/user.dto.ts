79eb0aae46a8a9a24d7e1f76ea9e478ed3b8a02d
Hiebeler <emanuel.hiebeler@gmail.com>
2025-07-30 17:22:33 +0200

    return null if no profile image exsists

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -4,7 +4,7 @@ export class UserDto {
   id: number;
   email: string;
   name: string;
-  profile_image: string;
+  profile_image: string | null;
   communities: CommunityDto[] | null;
   color: string;
   isAdmin?: boolean;
@@ -15,7 +15,7 @@ export class UserDto {
     this.email = user.email;
     this.name = user.name;
     const baseUrl = `${process.env.PROTOCOL}${process.env.HOST}`;
-    this.profile_image = user.profile_image ? `${baseUrl}${user.profile_image}` : '';
+    this.profile_image = user.profile_image ? `${baseUrl}${user.profile_image}` : null;
     this.communities = user.communities
       ? // biome-ignore lint/suspicious/noExplicitAny: could be diffrent user types from prisma
         user.communities.map((c: any) => new CommunityDto(c))
