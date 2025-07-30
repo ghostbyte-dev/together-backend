7e8502f0950110d92cbed97beb2b1872baaf8708
Hiebeler <emanuel.hiebeler@gmail.com>
2025-07-30 19:09:57 +0200

    fix size limit json

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use('/uploads', express.static('uploads'));
 
 const authRoute = require('./routes/auth.route');
