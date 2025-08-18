4069352934d5a77bc45e75def0381cad8c02a4ac
Hiebeler <emanuel.hiebeler@gmail.com>
2025-08-18 12:15:47 +0200

    fix length checks for feedback

--- src/services/feedback.service.ts
+++ src/services/feedback.service.ts
@@ -8,9 +8,9 @@ export class FeedbackService {
   constructor(@inject(PrismaService) private prisma: PrismaService) {}
 
   async create(feedback: string, userId: number) {
-    if (feedback.length <= 3) {
+    if (feedback.length < 3) {
       throw new ApiError('Too short feedback, min length is 3 characters', 400);
-    } else if (feedback.length >= 1000) {
+    } else if (feedback.length > 1000) {
       throw new ApiError('Too long feedback, max length is 1000 characters', 400);
     }
     await this.prisma.feedback.create({
